97eba3d1a04d7bb7b3ffd2b53faed5e33a02d348
jamesengleback <jamesengleback@hotmail.co.uk>
2022-08-08 16:31:56 +0100

    update bashrc to export EDITOR=nvim, install-debian.sh comment out desktop install stuff, better for servers

--- install-debian.sh
+++ install-debian.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-sudo apt update && sudo apt upgrade -y && sudo apt install neovim zathura alacritty sxiv feh suckless-tools xcompmgr libxinerama-dev libxft-dev -y
+sudo apt update && sudo apt upgrade -y && sudo apt install neovim htop -y
+# desktop stuff
+# sudo apt install zathura alacritty sxiv feh suckless-tools xcompmgr libxinerama-dev libxft-dev -y
 
 # https://github.com/junegunn/vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
